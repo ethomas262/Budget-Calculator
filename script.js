7e6e62ad3dae205badeb8af649a4cbecf66c5d75
Elijah Thomas <elijah@Zackerys-MacBook-Pro-8.local>
2023-02-19 22:36:38 -0500

    added bill display function to script

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-const name = prompt("What is your name?")
+const addBill = () =>{
+    let parentForm = document.getElementById('bill-adder')
 
-console.log(`Hello ${name}! Hope your having a great day!`)
+
+    let bill = []
+
+    bill.push(document.getElementById('bill-name').value)
+
+   
+
+    let new_bill = document.createElement('li')
+    new_bill.id = bill[1]
+    new_bill.InnerHTML = `${bill[1]} : ${bill[0]} : $${bill[2]}` 
+
+    let parentNode = document.getElementById('bill_display')
+
+    parentNode.appendChild(new_bill)
+
+
+
+    /*let clear_bill_button = document.createElement('input')
+    clear_bill_button.type = 'button'
+    clear_bill_button.innerHTML = 'X'
+    clear_bill_button.style.backgroundColor = 'red'
+    clear_bill_button.style.color = 'white'
+    
+
+    const remove_bill = ()=> {
+        parentNode.removeChild(new_bill)
+    }
+
+    new_bill.innerHTML +=  clear_bill_button*/
+
+    
+    
+}
